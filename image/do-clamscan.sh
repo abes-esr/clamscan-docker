4a877cc25ca86bc9975789e6e2a1d83e31da217b
Stéphane Gully <stephane.gully@gmail.com>
2021-04-11 20:08:15 +0200

    fix: check it SMTP_HOST is empty and do not send any alert mail if it is the case

--- image/do-clamscan.sh
+++ image/do-clamscan.sh
@@ -8,6 +8,7 @@ clamscan --recursive=yes --allmatch=yes ${FOLDER_TO_SCAN} | tee /tmp/clamscan.lo
 grep "Infected files: 0" /tmp/clamscan.log >/dev/null
 SOMETHING_IS_INFECTED=$?
 if [ "$SOMETHING_IS_INFECTED" != "0" ]; then
+  if [ "$SMTP_HOST" != "" ]; then
   echo "-> Infected: send an alert email to ${ALERT_MAILTO}"
   echo "To: ${ALERT_MAILTO}
 From: noreply@${SMTP_MAILDOMAIN}
@@ -15,4 +16,7 @@ Subject: ${ALERT_SUBJECT}
 
 $(cat /tmp/clamscan.log)" \
   | msmtp ${ALERT_MAILTO}
+  else
+    echo "-> Infected: but do not send any alert email because SMTP_HOST is empty"    
+  fi
 fi

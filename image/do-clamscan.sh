a88ad6cfb3ee0943aa7efabe41bf30f8ebced824
Stéphane Gully <stephane.gully@gmail.com>
2021-04-26 21:21:28 +0200

    fix eol

--- image/do-clamscan.sh
+++ image/do-clamscan.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ${FOLDER_TO_SCAN}
 
-echo -n "-> [$(date '+%Y-%m-%d %H:%M:%S')] - Scanning $FOLDER_TO_SCAN"
+echo "-> [$(date '+%Y-%m-%d %H:%M:%S')] - Scanning $FOLDER_TO_SCAN"
 clamscan --recursive=yes --allmatch=yes ${FOLDER_TO_SCAN} | tee /tmp/clamscan.log
 
 grep "Infected files: 0" /tmp/clamscan.log >/dev/null

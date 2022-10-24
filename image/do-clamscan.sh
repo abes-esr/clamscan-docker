db988bb4391cafa536f5c374d3d75707b1eee36f
Stéphane Gully <gully@abes.fr>
2022-10-24 13:54:49 +0200

    fix: sometimes clamscan.log can be empty (ex: if container restarted), this state should not generate a mail virus alert

--- image/do-clamscan.sh
+++ image/do-clamscan.sh
@@ -30,16 +30,18 @@ clamscan $CLAMSCAN_OPTIONS /tmp/new-files-to-scan/ | tee /tmp/clamscan.log
 grep "Infected files: 0" /tmp/clamscan.log >/dev/null
 SOMETHING_IS_INFECTED=$?
 if [ "$SOMETHING_IS_INFECTED" != "0" ]; then
-  if [ "$SMTP_HOST" != "" ]; then
-  echo "-> Infected: send an alert email to ${ALERT_MAILTO}"
-  echo "To: ${ALERT_MAILTO}
+  if [ "$(grep '[^[:space:]]' /tmp/clamscan.log)" != "" ]; then
+    if [ "$SMTP_HOST" != "" ]; then
+      echo "-> Infected: send an alert email to ${ALERT_MAILTO}"
+      echo "To: ${ALERT_MAILTO}
 From: noreply@${SMTP_MAILDOMAIN}
 Subject: ${ALERT_SUBJECT}
 
 $(cat /tmp/clamscan.log)" \
-  | msmtp ${ALERT_MAILTO}
-  else
-    echo "-> Infected: but do not send any alert email because SMTP_HOST is empty"    
+      | msmtp ${ALERT_MAILTO}
+    else
+      echo "-> Infected: but do not send any alert email because SMTP_HOST is empty"    
+    fi
   fi
 fi
 
